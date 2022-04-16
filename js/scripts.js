acaad64b5552ddd011c653d8bc839ae7d401b487
josecarlosgt <charlygt@gmail.com>
2022-04-15 19:24:09 -0700

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,6 @@
  * CIS 5620: Authoring Websites                                      *
  *********************************************************************/
 
-/* 
-  Avoid creating global variables by implementing all the application
-  inside an Immediately Invoked Function Expression (IIFE).
-*/
-(() => {
-
 // Data structure containing all product data displayed on the webpage
 const PRODUCTS = {
     product_1: {
@@ -17,55 +11,55 @@ const PRODUCTS = {
         description: "Hand-carved traditional Maori style bone Koru pendant",
         price: 50,
         stars: 1,
-        image: "assets/img/products/pendant.jpeg" 
+        image: "assets/img/products/pendant.jpeg"
     }, product_2: {
         id: "product_2",
         name: "Model War Canoe",
         description: "Maori model war canoe of carved wood, with a raised stern and a prow in the shape of a human head",
         price: 200,
         stars: 5,
-        image: "assets/img/products/canoe.jpeg"  
+        image: "assets/img/products/canoe.jpeg"
     }, product_3: {
         id: "product_3",
         name: "Maori Warrior Figure",
         description: "Vintage prominent Maori Tekoteko warrior figure",
         price: 30,
         stars: 4,
-        image: "assets/img/products/figure.jpeg" 
+        image: "assets/img/products/figure.jpeg"
     }, product_4: {
         id: "product_4",
         name: "Handicraft Brass Flower Vase",
         description: "This fine Maori work is a handcrafted piece of brassware considered highly attractive and artistic",
         price: 150,
         stars: 2,
-        image: "assets/img/products/vase.jpeg" 
+        image: "assets/img/products/vase.jpeg"
     }, product_5: {
         id: "product_5",
         name: "Natural Paua Shell Beads",
         description: "Approximately 15 to 17 Abalone 15x25 mm beads on a 15-inch strand",
         price: 50,
         stars: 3,
-        image: "assets/img/products/beats.jpeg"  
+        image: "assets/img/products/beats.jpeg"
     }
 };
 
- /**
-    Generates the HTML for displaying one product given its id in the
-    PRODUCTS object. This function follows a clone-find-update approach:
-    1. CLONE an HTML element to use as a template
-    2. FIND the elements using selectors
-    3. UPDATE the elements to customize their content
+/**
+   Generates the HTML for displaying one product given its id in the
+   PRODUCTS object. This function follows a clone-find-update approach:
+   1. CLONE an HTML element to use as a template
+   2. FIND the elements using selectors
+   3. UPDATE the elements to customize their content
 
-    @param    {number} productId An identifier in the PRODUCTS object to display
+   @param    {number} productId An identifier in the PRODUCTS object to display
 
-    @returns  {string} A string with the HTML of the product.
+   @returns  {string} A string with the HTML of the product.
 */
 function getProductHTML(productId) {
     // Obtain product data from the PRODUCTS object
-    const product = PRODUCTS[productId];   
+    const product = PRODUCTS[productId];
 
     // CLONE an HTML element to use as a template
-    const productHTML = $( "#product-template" ).clone();
+    const productHTML = $("#product-template").clone();
 
     // Delete id to avoid duplicates
     productHTML.prop('id', '');
@@ -73,15 +67,15 @@ function getProductHTML(productId) {
     // FIND and UPDATE the product's name
     productHTML.find(".product-name").text(product.name);
     productHTML.find(".product-price").text(product.price.toFixed(2));
- 
+
     // FIND and UPDATE the product's image properties
     productHTML.find("img").
         prop("src", product.image).
         prop("alt", product.name);
 
-     // Customize the product's reviews    
+    // Customize the product's reviews    
     const starHTML = productHTML.find(".product-reviews").find("div");
-    for(let starsCounter = 2; starsCounter <= product.stars; starsCounter++) {
+    for (let starsCounter = 2; starsCounter <= product.stars; starsCounter++) {
         const newStartHTML = starHTML.clone();
         productHTML.find(".product-reviews").append(newStartHTML);
     }
@@ -95,15 +89,12 @@ function getProductHTML(productId) {
 /**
     Show all products in the application's homepage
 
-    @param    {array} products An array of objects containing all the products to be displayed
     @returns  No value.
 */
-function showProducts(products) {
-    // Sort products by the number of stars in descending order
-    products.sort((product1, product2) => product2.stars - product1.stars);
-
+function showProducts() {
     // Traverse the products object
-    for(let product of products) {
+    for (let productId in PRODUCTS) {
+        const product = PRODUCTS[productId];
 
         // Generate each product's HTML
         const productHTML = getProductHTML(product.id);
@@ -121,12 +112,4 @@ function showProducts(products) {
     }
 }
 
-/* .ready(): https://api.jquery.com/ready/
-    The .ready() method allow us to run JavaScript code as soon as the page's Document Object Model (DOM)
-    becomes SAFE to manipulate. 
-*/
-$(document).ready(function(){
-    showProducts(Object.values(PRODUCTS));
-});
-
-})();
+showProducts();
